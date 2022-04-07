4c736031721d5e40c5810b80bd521bc405df94f6
mrr-r <66952858+mrr-r@users.noreply.github.com>
2022-04-07 17:51:06 +0430

    ایراد با ماژول تقویم انتخابی
    این کد با ماژول تقویم datepicker تداخل ایجاد کرده بود.

--- l10n_ir_base/models/res_user.py
+++ l10n_ir_base/models/res_user.py
@@ -4,14 +4,14 @@ from odoo import _, api, fields, models
 class ResUser(models.Model):
     _inherit = "res.users"
 
-    calendar = fields.Char(
-        string="Calendar",
-        size=10,
-        help="user current calendar",
-        readonly=True,
-        translate=True,
-        compute="_compute_calendar",
-    )
+    # calendar = fields.Char(
+        # string="Calendar",
+        # size=10,
+        # help="user current calendar",
+        # readonly=True,
+        # translate=True,
+        # compute="_compute_calendar",
+    # )
 
     @api.depends("user_id")
     def _compute_calendar(self):
